e336ce24df72774887b1da2a95b302d5cd67a5c8
TranTam <tamvaa1306@gmail.com>
2022-09-14 11:56:26 +0700

    get data from store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,35 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import Task from "../models/TaskModel";
+import { findIndex } from "lodash";
 
 export default createStore({
   state: {
-  },
-  getters: {
+    tasks: [
+      {
+        name: "Demo for Vue 3 with TS",
+        completed: false,
+      },
+      {
+        name: "Learning about TS basic",
+        completed: false,
+      },
+    ] as Task[],
   },
   mutations: {
+    setTask: (state, task) => state.tasks.push(task),
+
+    deleteTask(state, task): void {
+      let taskIndex = findIndex(state.tasks, task);
+      state.tasks.splice(taskIndex, ++taskIndex);
+    },
+
+    completeTask(state, task): void {
+      const taskIndex = findIndex(state.tasks, task);
+      state.tasks[taskIndex].completed = true;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+
+  actions: {},
+
+  modules: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,61 @@
 <template>
   <div class="home">
-    <h1>Task List</h1>
+    <el-table :data="tasks" border style="width: 100%">
+      <el-table-column prop="name" label="Nhiệm vụ" width="500">
+      </el-table-column>
+      <el-table-column
+        prop="completed"
+        label="Trạng thái"
+        :formatter="formatStatus"
+        width="200"
+      >
+      </el-table-column>
+      <el-table-column label="Thao tác">
+        <template #default="scope">
+          <el-button @click="setTaskComplete(scope.row)" type="text" size="mini"
+            >Completed</el-button
+          >
+          <el-button
+            @click="deleteTask(scope.row)"
+            type="text"
+            size="mini"
+            style="color: red"
+            >Delete</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Task from "@/models/TaskModel";
 
 export default defineComponent({
   name: "HomeView",
+  data() {
+    return {
+      tasks: [] as Task[],
+    };
+  },
+
+  methods: {
+    setTaskComplete(task: Task): void {
+      this.$store.commit("completeTask", task);
+    },
+
+    deleteTask(task: Task) {
+      this.$store.commit("deleteTask", task);
+    },
+
+    formatStatus(task: Task): string {
+      return task.completed ? "Đã hoàn thành" : "Chưa hoàn thành";
+    },
+  },
+
+  mounted() {
+    this.tasks = this.$store.state.tasks;
+  },
 });
 </script>
