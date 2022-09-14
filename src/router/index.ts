20121a5c9079a7fdc1f2fc22fc83e198eb0020b3
TranTam <tamvaa1306@gmail.com>
2022-09-14 11:38:02 +0700

    setup router

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AddTask.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/AddTask.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an addtask page</h1>
+  </div>
+</template>
