74f6d8684498182240e9c0536a924a00fd125157
TranTam <tamvaa1306@gmail.com>
2022-09-14 11:58:53 +0700

    addTask view

--- src/views/AddTask.vue
+++ src/views/AddTask.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an addtask page</h1>
+  <div class="add-task">
+    <el-card class="box-card">
+      <el-input
+        type="textarea"
+        :rows="2"
+        size="small"
+        placeholder="Enter your task"
+        v-model="taskName"
+        @keyup.enter="addTask()"
+      >
+      </el-input>
+
+      <el-row>
+        <el-button type="primary">Submit</el-button>
+      </el-row>
+    </el-card>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Task from "@/models/TaskModel";
+
+export default defineComponent({
+  name: "AddTask",
+  data() {
+    return {
+      taskName: "",
+    };
+  },
+  methods: {
+    addTask(): void {
+      if (this.taskName !== "") {
+        const newTask = new Task(this.taskName);
+        this.$store.commit("setTask", newTask);
+        this.taskName = "";
+        this.$router.push("/");
+      }
+    },
+  },
+});
+</script>
